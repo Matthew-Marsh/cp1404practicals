180ae2e92b34e16d29148cfa4ffc63e053b9a38c
Matthew-Marsh <Sarhnak02>
2021-03-11 18:25:12 +1000

    Refactor broken score for functions and add random result

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,15 +2,28 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
+def main():
+    score = float(input("Enter score: "))
 
-print_message = "Invalid score"
-if score <= 100 and score >= 90:
-    print_message = "Excellent"
-elif score >= 50 and score < 90:
-    print_message = "Passable"
-elif score < 50 and score >= 0:
-    print_message = "Bad"
+    print_message = determine_result(score)
+    print("\t{}".format(print_message))
 
-print(print_message)
+    score = random.randint(0, 101)
+    print_message = determine_result(score)
+    print("Score {}: \n\t{}".format(score,print_message))
+
+
+def determine_result(score):
+    print_message = "Invalid score"
+    if score <= 100 and score >= 90:
+        print_message = "Excellent"
+    elif score >= 50 and score < 90:
+        print_message = "Passable"
+    elif score < 50 and score >= 0:
+        print_message = "Bad"
+    return print_message
+
+
+main()
