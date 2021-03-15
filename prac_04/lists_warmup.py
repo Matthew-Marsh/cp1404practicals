c4f4c621a7a7faaa518850eaac85886c53735b1a
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-15 17:36:33 +1000

    Add corrections to answers in warmup

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,8 +3,8 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # numbers[0]: 3
 # numbers[-1]: 2
 # numbers[3]: 1
-# numbers[:-1]: [3, 1, 4, 1, 5, 9, 2]
-# numbers[3:4]: [1, 5]
+# numbers[:-1]: [3, 1, 4, 1, 5, 9, 2] x [3, 1, 4, 1, 5, 9]
+# numbers[3:4]: [1, 5] x [1]
 # 5 in numbers: True
 # 7 in numbers: False
 # "3" in numbers: False
