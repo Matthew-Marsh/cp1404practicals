a6cbdcba910f7e640c21180a06f282f0722309c1
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-15 17:41:19 +1000

    Add expressions to warmup file

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -9,3 +9,13 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 # 7 in numbers: False
 # "3" in numbers: False
 # numbers + [6, 5, 3]: [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+# Python Expressions
+# 1:
+numbers[0] = "ten"
+# 2:
+numbers[-1] = 1
+# 3:
+numbers[2:]
+# 4.
+9 in numbers
