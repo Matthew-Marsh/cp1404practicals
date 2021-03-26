283563b6bc6626243b896d94a2e8eb05b4e0683c
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-26 18:00:40 +1000

    Add second task for list exercises

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,13 @@ print("The smallest number is {}".format(min(numbers)))
 print("The largest number is {}".format(max(numbers)))
 print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
 
+# 2.
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+username = input("Username: ")
+while username not in usernames:
+    print("Access denied")
+    username = input("Username: ")
+print("Access granted")
