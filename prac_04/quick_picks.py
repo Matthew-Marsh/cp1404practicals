0bad1272bcb5b0b78094ca41b10213b135068a61
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-26 18:42:32 +1000

    Fix input request message to match sample

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -4,7 +4,7 @@ NUMBER_OF_NUMBERS = 6
 LARGEST_NUMBER = 45
 SMALLEST_NUMBER = 1
 
-number_of_picks = int(input("Enter number of quick picks: "))
+number_of_picks = int(input("How many quick picks? "))
 random_numbers = []
 for number in range(number_of_picks):
     random_numbers = [random.randint(SMALLEST_NUMBER, LARGEST_NUMBER) for number in range(NUMBER_OF_NUMBERS)]
