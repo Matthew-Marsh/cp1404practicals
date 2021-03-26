b6fad214d2a0f5f4912e99b290f22c5c2c656a50
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-26 18:41:35 +1000

    Add list comprehension for each pick and complete program

---
+++ prac_04/quick_picks.py
@@ -0,0 +1,14 @@
+import random
+
+NUMBER_OF_NUMBERS = 6
+LARGEST_NUMBER = 45
+SMALLEST_NUMBER = 1
+
+number_of_picks = int(input("Enter number of quick picks: "))
+random_numbers = []
+for number in range(number_of_picks):
+    random_numbers = [random.randint(SMALLEST_NUMBER, LARGEST_NUMBER) for number in range(NUMBER_OF_NUMBERS)]
+    random_numbers.sort()
+    for number in random_numbers:
+        print("{:>2}".format(number), end=" ")
+    print()
