9855b7d629a32741f6506aec55e21c8a518ac851
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-26 17:45:07 +1000

    Change get data function to return list of lists and refactor data print into a print function

--- prac_04/subject_reader.py
+++ prac_04/subject_reader.py
@@ -8,22 +8,24 @@ FILENAME = "subject_data.txt"
 
 def main():
     data = get_data()
-    print(data)
+    print_data(data)
+
+
+def print_data(data):
+    for subject in data:
+        print("{} is taught by {:12} and has {:>3} students".format(subject[0], subject[1], subject[2]))
 
 
 def get_data():
     """Read data from file formatted like: subject,lecturer,number of students."""
     input_file = open(FILENAME)
+    subjects = []
     for line in input_file:
-        print(line)  # See what a line looks like
-        print(repr(line))  # See what a line really looks like
-        line = line.strip()  # Remove the \n
-        parts = line.split(',')  # Separate the data into its parts
-        print(parts)  # See what the parts look like (notice the integer is a string)
-        parts[2] = int(parts[2])  # Make the number an integer (ignore PyCharm's warning)
-        print(parts)  # See if that worked
-        print("----------")
+        line = line.strip()
+        parts = line.split(',')
+        parts[2] = int(parts[2])
+        subjects.append(parts)
     input_file.close()
-
+    return subjects
 
 main()
