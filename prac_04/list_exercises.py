37813ede90c0747ab64f5842cdf23f2574771a2f
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-03-26 17:56:15 +1000

    Add first task of the list exercises

---
+++ prac_04/list_exercises.py
@@ -0,0 +1,11 @@
+# 1.
+numbers = []
+for number in range(5):
+    number = int(input("Number: "))
+    numbers.append(number)
+print("The first number is {}".format(numbers[0]))
+print("The last number is {}".format(numbers[-1]))
+print("The smallest number is {}".format(min(numbers)))
+print("The largest number is {}".format(max(numbers)))
+print("The average of the numbers is {}".format(sum(numbers) / len(numbers)))
+
