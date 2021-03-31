5b8cdaaae927b318531e1164acaa4b1a84ff3a8e
Matthew-Marsh <Sarhnak02>
2021-03-31 11:30:53 +1000

    Update program for cleaner code as per pylint

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,8 @@
+"""
+Program will request a sentence from the user and count the number of occurrences of each word
+before printing a sorted dictionary
+"""
+
 sentence = input("Text: ").lower()
 words = sentence.split()
 
@@ -9,7 +14,5 @@ for word in words:
         word_to_count[word] = 1
 
 maximum_word_length = max([len(word) for word in words])
-words = [word for word in word_to_count]
-for word in sorted(words):
+for word in sorted(word_to_count):
     print("{:{}} : {}".format(word, maximum_word_length, word_to_count[word]))
-
