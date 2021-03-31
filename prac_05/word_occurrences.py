473918cf7de711c376ea976efcd120d4796db06b
Matthew-Marsh <Sarhnak02>
2021-03-31 11:10:07 +1000

    Add for loop that counts word occurrences in words list

---
+++ prac_05/word_occurrences.py
@@ -0,0 +1,10 @@
+sentence = input("Text: ")
+words = sentence.split()
+
+word_to_count = {}
+for word in words:
+    try:
+        word_to_count[word] += 1
+    except KeyError:
+        word_to_count[word] = 1
+
