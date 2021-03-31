0183c9d2ccb87710cfc6f169f0efb665413cd210
Matthew-Marsh <Sarhnak02>
2021-03-31 11:22:08 +1000

    Add dictionary printing loop

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,4 +1,4 @@
-sentence = input("Text: ")
+sentence = input("Text: ").lower()
 words = sentence.split()
 
 word_to_count = {}
@@ -7,4 +7,9 @@ for word in words:
         word_to_count[word] += 1
     except KeyError:
         word_to_count[word] = 1
+
+maximum_word_length = max([len(word) for word in words])
+words = [word for word in word_to_count]
+for word in sorted(words):
+    print("{:{}} : {}".format(word, maximum_word_length, word_to_count[word]))
 
