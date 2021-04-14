54866ef85f72362696b4534104e5093d282d2660
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-04-14 09:35:27 +0100

    Update with some format fixes and remove unnecessary testing

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -9,12 +9,11 @@ def main():
     guitar_name = input("Name: ")
     while guitar_name != "":
         guitar_year = int(input("Year: "))
-        guitar_cost = float(input("Cost: "))
+        guitar_cost = float(input("Cost: $"))
         guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+        print("{} ({}) : ${:.2f} added.".format(guitar_name, guitar_year, guitar_cost))
+        print()
         guitar_name = input("Name: ")
-
-    # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    # guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     print("These are my guitars:")
     for i, guitar in enumerate(guitars, 1):
