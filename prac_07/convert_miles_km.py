fecbeec305c5a3223245d8c8dcb9b02f15c0a0ec
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-04-28 10:27:01 +0100

    Update convert miles to km file with correct import for StringProperty

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -6,7 +6,7 @@ Matthew Marsh
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
-from kivy.app import StringProperty
+from kivy.properties import StringProperty
 
 __author__ = "Matthew Marsh"
 
@@ -14,25 +14,25 @@ MILE_TO_KM = 1.61
 
 
 class ConvertMilesToKmApp(App):
-    """ ConvertMilesToKm is a kivy app that takes a number of kms and converts it to miles"""
+    """ ConvertMilesToKm is a kivy app that takes a number of kms and converts it to miles. """
     kilometres = StringProperty()
 
     def build(self):
-        """ build the app from the kv file """
+        """ Build the app from the kv file. """
         Window.size = (600, 300)
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
         return self.root
 
     def calculate_button_press(self):
-        """ calculate the km converted from the input miles """
+        """ Calculate the km converted from the input miles. """
         try:
-            self.kilometres = str(int(self.root.ids.input_number.text) * MILE_TO_KM)
+            self.kilometres = int(self.root.ids.input_number.text) * MILE_TO_KM
         except ValueError:
             self.kilometres = '0.0'
 
     def handle_increment(self, number=0, increment=1):
-        """ increase the input number by the increment """
+        """ Increase the input number by the increment. """
         try:
             if number == "":
                 number = 0
