01bf68963f37070ddb160065c5e5f07b98fd7c4c
Matt Marsh <matthew.marsh@my.jcu.edu.au>
2021-05-07 12:37:22 +1000

    Update unreliable car test file with constants

--- prac_08/unreliable_car_test.py
+++ prac_08/unreliable_car_test.py
@@ -1,13 +1,17 @@
 """ Determine if car will drive a distance based on reliability. """
 from prac_08.unreliable_car import UnreliableCar
+import random
+MINIMUM_DRIVE = 1
+MAXIMUM_DRIVE = 200
 
 
 def main():
-    """Test car reliability class with car instance."""
+    """Test car reliability class with car instances."""
     honda = UnreliableCar('Honda', 200, 20)
-    toyota = UnreliableCar('Toyota', 200, 90)
-    honda_distance = honda.drive(100)
-    toyota_distance = toyota.drive(150)
+    toyota = UnreliableCar('Toyota', 250, 90)
+
+    honda_distance = honda.drive(random.randint(MINIMUM_DRIVE, MAXIMUM_DRIVE))
+    toyota_distance = toyota.drive(random.randint(MINIMUM_DRIVE, MAXIMUM_DRIVE))
     if honda_distance == 0:
         print("Honda was a piece of junk and couldn't drive", end="")
         honda_drove = False
